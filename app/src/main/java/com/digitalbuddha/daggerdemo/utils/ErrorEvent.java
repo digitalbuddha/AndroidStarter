8d52ede1b2322f47b9e1b8cb685ff0bf969444a0
MikeN <mike@touchlab.co>
2014-09-24 20:38:41 -0400

    more job stuff

---
+++ app/src/main/java/com/digitalbuddha/daggerdemo/utils/ErrorEvent.java
@@ -0,0 +1,8 @@
+package com.digitalbuddha.daggerdemo.utils;
+
+/**
+ * Created by MikeN on 9/24/14.
+ */
+public class ErrorEvent {
+    String message;
+}
--- app/src/test/java/com/digitalbuddha/daggerdemo/APITest.java
+++ app/src/test/java/com/digitalbuddha/daggerdemo/APITest.java
@@ -1,15 +1,25 @@
 package com.digitalbuddha.daggerdemo;
 
+import com.android.volley.toolbox.RequestFuture;
+import com.digitalbuddha.daggerdemo.rest.SavingsTypesRequest;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.annotation.Config;
+
+import java.util.List;
 
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(emulateSdk = 18)
 public class APITest extends BaseTestCase
 {
-
-
-
-
+    @Test
+    public void tesGetSavingTypesJob() throws Exception {
+        RequestFuture<List> future=RequestFuture.newFuture();
+        SavingsTypesRequest savingsTypesRequest= new SavingsTypesRequest(future,future,"google.com");
+        savingsTypesRequest.invoke();
+        future.get();
+        assertNotNull(savingsTypesRequest.savingsTypes);
+    }
 }
