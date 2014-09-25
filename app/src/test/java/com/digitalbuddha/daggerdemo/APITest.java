c6ba42d035bbd6b78c6c4ca56c8f041247bd3c9e
MikeN <mike@touchlab.co>
2014-09-24 20:04:29 -0400

    testing with roboelectric!

---
+++ app/src/test/java/com/digitalbuddha/daggerdemo/APITest.java
@@ -0,0 +1,15 @@
+package com.digitalbuddha.daggerdemo;
+
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(emulateSdk = 18)
+public class APITest extends BaseTestCase
+{
+
+
+
+
+}
